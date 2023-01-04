ebe07f8c2b7efeca8b35e58f32b297cf75f47e11
Cameron O'Brien <obriecam@oregonstate.edu>
2023-01-03 21:58:50 -0800

    Refactored repository.

--- rankYourWrapped/api/serializers.py
+++
@@ -1,7 +0,0 @@
-from rest_framework import serializers
-from .models import LeaderboardEntry
-
-class LeaderboardEntrySerializer(serializers.ModelSerializer):
-    class Meta:
-        model = LeaderboardEntry
-        fields = ('id', 'first_name', 'last_name', 'username', 'score')
--- rankYourWrapped/api/urls.py
+++
@@ -1,6 +0,0 @@
-from django.urls import path
-from .views import LeaderboardEntryView
-
-urlpatterns = [
-    path('leaderboard_entries', LeaderboardEntryView.as_view())
-]
